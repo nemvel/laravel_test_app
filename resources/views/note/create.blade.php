12c7e7319219959b4b255336f9fa6259830b544a
Nemanja Velickovic <nvelickovickg@gmail.com>
2025-01-27 17:09:48 +0100

    Implement CRUD operations on notes

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -2,6 +2,7 @@
     <div class="note-container single-note">
         <h1>Create new note</h1>
         <form action="{{ route('note.store') }}" method="POST" class="note">
+            @csrf
             <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here">
             </textarea>
             <div class="note-buttons">
